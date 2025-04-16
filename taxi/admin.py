b5a4fd0fe2b49b62fd7d550d241adf397ddbb846
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2025-04-16 20:44:37 +0300

    Recreated .gitignore

--- taxi/admin.py
+++ taxi/admin.py
@@ -9,12 +9,12 @@ from taxi.models import Manufacturer, Driver, Car
 class ManufacturerAdmin(ModelAdmin):
     pass
 
+
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = ("license_number",)
     fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
-
 
 
 @admin.register(Car)
